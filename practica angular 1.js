0405927047348df0ea127b26f7f0818c631890db
José Alvis <jalvis1491@gmail.com>
2017-07-19 11:11:30 -0400

    Buen trabajo
    Agregue tu código dentro  de un IIFE , para que te vallas familiarizando con eso.

--- practica angular 1.js
+++ practica angular 1.js
@@ -1,6 +1,12 @@
+(function(){
+  'use strict'
+
 var nombre = "victor"; 
 var apellido = "almonte"; 
-var edad = 22; var profecion = "tec. artes graficas, estudiante de sistema y futuro FRONT-END"; 
+var edad = 22; 
+var profecion = "tec. artes graficas, estudiante de sistema y futuro FRONT-END"; 
 var nivel = 3;
 
 console.log("soy "+nombre+" "+apellido+" mi edad "+edad+" soy "+profecion+" mi nivel en programacion es "+nivel);
+
+})();
